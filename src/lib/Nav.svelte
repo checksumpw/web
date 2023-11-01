5e34f76fd873520a983a86c57162cd610f05419a
GregiPlays <63959442+GregiPlays@users.noreply.github.com>
2023-11-01 12:32:33 +0100

    Update Nav.svelte
    Change Login Method

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,7 @@
 <div class="text-white flex flex-row justify-between items-center px-4 py-2 bg-gradient-to-r from-rose-600 to-rose-800">
     <h1 class="text-2xl font-bold">Hikari</h1>
     <div class="flex flex-row justify-between items-center space-x-2">
-        <a href="/register" class="text-white bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Register</a>
+        <a href="http://hikari-anime.com:8056/invite/wtsKm68rvzukJtECfQ2at3" class="text-white bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Register</a> 
         <a href="https://watch.hikari-anime.com/" class="text-white bg-gradient-to-r from-rose-600 to-rose-800 px-4 py-2 rounded-lg hover:from-rose-500 hover:to-rose-600">Login</a> 
     </div>
 </div>
