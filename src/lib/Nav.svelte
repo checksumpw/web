f4e5e84f98bef84a55970b7ae2c745572690bd87
GregiPlays <63959442+GregiPlays@users.noreply.github.com>
2023-11-23 15:26:30 +0100

    Update Nav.svelte
    Undo settings

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,14 +1,5 @@
 <div class="text-white flex flex-row justify-between items-center px-4 py-2 bg-gradient-to-r from-rose-600 to-rose-800">
     <h1 class="text-2xl font-bold">Hikari</h1>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6TP10ZKDJL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6TP10ZKDJL');
-</script>
     <div class="flex flex-row justify-between items-center space-x-2">
         <a href="https://discord.com/invite/UAPNesVmQ3" class="text-primary bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Discord Community</a> 
         <a href="http://hikari-anime.com:8056/invite/wtsKm68rvzukJtECfQ2at3" class="text-white bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Register</a> 
