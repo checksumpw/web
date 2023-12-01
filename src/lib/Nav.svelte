36df21de72bbd4ac389b6573fa2ddc48859cdbcd
GregiPlays <63959442+GregiPlays@users.noreply.github.com>
2023-12-01 14:25:57 +0100

    Update Nav.svelte
    Update Register Link

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,7 +2,7 @@
     <h1 class="text-2xl font-bold">Hikari</h1>
     <div class="flex flex-row justify-between items-center space-x-2">
         <a href="https://discord.com/invite/UAPNesVmQ3" class="text-primary bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Discord Community</a> 
-        <a href="http://hikari-anime.com:8056/invite/wtsKm68rvzukJtECfQ2at3" class="text-white bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Register</a> 
+        <a href="http://hikari-anime.com:8056/invite/kamUfSWwccWbWx7TxDBHua" class="text-white bg-rose-600 hover:bg-rose-500 px-4 py-2 rounded-lg">Register</a> 
         <a href="https://watch.hikari-anime.com/" class="text-white bg-gradient-to-r from-rose-600 to-rose-800 px-4 py-2 rounded-lg hover:from-rose-500 hover:to-rose-600">Login</a> 
     </div>
 </div>
